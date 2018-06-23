256be00e1e0737dddab6769b704169dcec4c13e8
mehmetgokhan <gokhan.iyidogan@gmail.com>
2018-06-22 20:58:12 -0500

    Selenium Project

---
+++ src/com/cbt/SeleniumDemo.java
@@ -0,0 +1,17 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+		public static void main (String[] args) {
+	
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\Selenium Dependencies\\Drivers\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("http://google.com");
+	}
+}
+  
